161487e0865334b6293711e84d4cd6dd3957aa33
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-11-16 00:57:28 +0200

    UPD: previous commit (fixed bug with joining the game)

--- 9. Client-Server BlackJack/server/src/controllers/rooms/joinRoom.js
+++ 9. Client-Server BlackJack/server/src/controllers/rooms/joinRoom.js
@@ -2,6 +2,34 @@ const Room = require('../../schemas/rooms');
 const createToken = require('../../helpers/createToken');
 const { nanoid } = require('nanoid');
 const Player = require('../../utils/Player');
+
+const addPlayerToRoom = async room => {
+  const player = new Player();
+  player.setCards(room.cards.splice(0, 2));
+  const playerId = nanoid();
+
+  const updatedRoom = await Room.findByIdAndUpdate(
+    room._id,
+    {
+      cards: room.cards,
+      $push: {
+        players: { ...player, uid: playerId },
+      },
+    },
+    { new: true }
+  );
+
+  const userToken = createToken(playerId);
+  const roomToken = createToken(room._id);
+
+  return {
+    roomToken,
+    userToken,
+    dealerCards: updatedRoom.dealerCards,
+    players: updatedRoom.players.map(player => delete player.uid && player),
+  };
+};
+
 const joinRoom = async (req, res) => {
   const { rid } = req.room;
   const uid = req?.user?.uid;
@@ -20,9 +48,12 @@ const joinRoom = async (req, res) => {
     const player = room.players.find(player => player.uid === uid);
 
     if (!player) {
-      return res
-        .status(400)
-        .json({ message: `You aren't a participant in this room ` });
+      if (room.started) {
+        return res.status(400).json({ message: 'Room already started' });
+      }
+
+      const result = await addPlayerToRoom(room);
+      return res.status(201).json(result);
     }
 
     const roomToken = createToken(rid);
@@ -36,30 +67,8 @@ const joinRoom = async (req, res) => {
     });
   }
 
-  const player = new Player();
-  player.setCards(room.cards.splice(0, 2));
-  const playerId = nanoid();
-
-  const updatedRoom = await Room.findByIdAndUpdate(
-    rid,
-    {
-      cards: room.cards,
-      $push: {
-        players: { ...player, uid: playerId },
-      },
-    },
-    { new: true }
-  );
-
-  const userToken = createToken(playerId);
-  const roomToken = createToken(rid);
-
-  return res.status(201).json({
-    roomToken,
-    userToken,
-    dealerCards: updatedRoom.dealerCards,
-    players: updatedRoom.players.map(player => delete player.uid && player),
-  });
+  const result = await addPlayerToRoom(room);
+  return res.status(201).json(result);
 };
 
 module.exports = joinRoom;
